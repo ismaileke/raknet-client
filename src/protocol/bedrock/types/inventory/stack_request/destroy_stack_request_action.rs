872278ca7ad9c3235861eaaa84dc80566dd90657
ismail <dev.1smail.eke34@gmail.com>
2026-09-07 23:23:35 +0300

    crafting_consume_input_stack_request_action and destroy_stack_request_action update

--- src/protocol/bedrock/types/inventory/stack_request/destroy_stack_request_action.rs
+++ src/protocol/bedrock/types/inventory/stack_request/destroy_stack_request_action.rs
@@ -1,16 +1,26 @@
 use binary_utils::binary::{Reader, Writer};
+use crate::protocol::bedrock::types::inventory::stack_request::item_stack_request_slot_info::ItemStackRequestSlotInfo;
 
 #[derive(serde::Serialize, Debug)]
-pub struct DestroyStackRequestAction {}
+pub struct DestroyStackRequestAction {
+    pub count: u8,
+    pub source: ItemStackRequestSlotInfo
+}
 
 impl DestroyStackRequestAction {
-    pub fn new() -> DestroyStackRequestAction {
-        DestroyStackRequestAction {}
+    pub fn new(count: u8, source: ItemStackRequestSlotInfo) -> DestroyStackRequestAction {
+        DestroyStackRequestAction { count, source }
     }
 
-    pub fn read(_stream: &mut Reader) -> DestroyStackRequestAction {
-        DestroyStackRequestAction {}
+    pub fn read(stream: &mut Reader) -> DestroyStackRequestAction {
+        DestroyStackRequestAction {
+            count: stream.get_u8(),
+            source: ItemStackRequestSlotInfo::read(stream),
+        }
     }
 
-    pub fn write(&self, _stream: &mut Writer) {}
+    pub fn write(&self, stream: &mut Writer) {
+        stream.put_u8(self.count);
+        self.source.write(stream);
+    }
 }
